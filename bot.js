b271248ef76d90684d73d5e333ea58af0e5722d6
RyanTorb <54119466+RyanTorb@users.noreply.github.com>
2020-01-28 12:52:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "Great question!"
+  botResponse = "Great question!";
 
   options = {
     hostname: 'api.groupme.com',
